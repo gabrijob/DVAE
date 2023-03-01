da0548d34152252ec4d7628f29303c1035502f95
gabrijob <gabrijob@gmail.com>
2023-03-01 12:15:21 +0100

    data with configs

--- dvae/dataset/offline_metrics_dataset.py
+++ dvae/dataset/offline_metrics_dataset.py
@@ -47,14 +47,13 @@ class OfflinePrometheusMetrics(Dataset):
         self.shuffle = shuffle
 
         # {'metric_1':[...], 'metric_2':[...], ..., 'metric_n':[...]}
-        self.metric_pool = {}
+        metric_pool = {}
         
         dirs = os.listdir(svc_datadir)
         files = []
         for dir in dirs:
             f_in_dir = os.listdir(svc_datadir+'/'+dir)
             files.extend([dir+'/'+f for f in f_in_dir if os.path.isfile(svc_datadir+'/'+dir+'/'+f)])
-        #print(*files, sep="\n")
         
         # For metric in metric_names:
         for fname in files:
@@ -67,37 +66,36 @@ class OfflinePrometheusMetrics(Dataset):
             # Insert time-series into metric_pool
             #metric = results[0]['metric']['__name__']
             metric = os.path.basename(fname).split('.')[0]
-            #print(metric)
             truncate_at = (len(results[0]['values']) // self.seq_len) * self.seq_len
-            if metric in self.metric_pool:
-                values = self.metric_pool[metric]
+            if metric in metric_pool:
+                values = metric_pool[metric]
                 # appending metrics of different experiments must be done carefuly as to avoid having the overlap of sequences
                 # for example, a sequence containing the end of an experiment and the beginning of another 
                 values.extend(results[0]['values'][:truncate_at]) 
-                self.metric_pool[metric] = values
+                metric_pool[metric] = values
             else:
-                self.metric_pool[metric] = results[0]['values'][:truncate_at]
+                metric_pool[metric] = results[0]['values'][:truncate_at]
 
         # Assert that every metric has the same number of samples
-        t_per_metric = [len(self.metric_pool[metric]) for metric in self.metric_pool.keys()]
+        t_per_metric = [len(metric_pool[metric]) for metric in metric_pool.keys()]
         if sum(t_per_metric) != (len(t_per_metric) * t_per_metric[0]):
             raise Exception("Metric data does not have the same size across all metrics.")
         
         # Preprocess metric matrix
-        self.metric_matrix = self.preprocess_data(split, t_per_metric)
+        self.metric_matrix = self.preprocess_data(metric_pool, split, t_per_metric)
 
         
     
-    def preprocess_data(self, split, t_per_metric):
+    def preprocess_data(self, metric_pool, split, t_per_metric):
         # Order metric pool by metric name
-        self.metric_pool = dict(sorted(self.metric_pool.items()))
+        metric_pool = dict(sorted(metric_pool.items()))
 
         # Compute sequence grouping
         self.compute_seqs(split=split, t_per_metric=t_per_metric)
 
         # Normalize metric values
-        np_metrics = np.empty((len(self.metric_pool.items()), t_per_metric[0]), dtype=np.float32)
-        for i,metric in enumerate(self.metric_pool.items()):
+        np_metrics = np.empty((len(metric_pool.items()), t_per_metric[0]), dtype=np.float32)
+        for i,metric in enumerate(metric_pool.items()):
             values = np.array(metric[1], dtype=np.float32)[:,1]
             np_metrics[i] = values
 
@@ -146,28 +144,5 @@ class OfflinePrometheusMetrics(Dataset):
         """
         seq_start, seq_end = self.valid_seq_list[index]
         sample_seq = self.metric_matrix[seq_start:seq_end, :]
-
-        #sample_seq = np.empty((self.seq_len, len(self.metric_pool.items())), dtype=np.float32)
-
-        # Variable metric in the shape of ('name', [(timestamp, value), ...])
-        #for metric in self.metric_pool.items():
-        #    name = metric[0]
-        #    sequence = np.array(metric[1][1][seq_start:seq_end]) 
-        #    sample[name] = sequence
-
-        #for i in range(seq_start, seq_end):
-        #    sample_i = []
-        #    for metric in self.metric_pool.items():
-        #        value = float(metric[1][i][1])
-        #        if (np.isnan(value)): # Check for pos-inf, neg-inf, nan
-        #            sample_i.append(0)
-        #        else:
-        #            sample_i.append(value)
-        #    sample_seq[i-seq_start] = np.array(sample_i, dtype=np.float32)
-            #print(sample_seq)
-            
-            #if ~np.isfinite(np.sum(sample_seq[i-seq_start])):
-            #    print(sample_seq)
-            #    print("At " + str(seq_start) + "," + str(seq_end)) 
 
         return sample_seq
